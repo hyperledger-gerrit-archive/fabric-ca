78497104fdbbdd48c031e96266a3a519f247771b
Saad Karim <skarim@us.ibm.com>
2017-01-09 11:33:43 -0500

    COP Client Configuration File
    COP client will now make use of a configuration file. This will
    simply the CLI commands on the client side. The config file will
    be passed in using -config file.
    
    Example:
    cop client enroll -config ../testdata/cop_client.json admin adminpw
    
    https://jira.hyperledger.org/browse/FAB-1622
    
    Also, resolves absolute pathing on the client side.
    
    https://jira.hyperledger.org/browse/FAB-1549
    
    Change-Id: I985ed5744eb8baef2c40ebedb3b1e0abf0fc1696
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- cli/server/server_test.go
+++ cli/server/server_test.go
@@ -21,7 +21,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
@@ -32,9 +31,9 @@ import (
 )
 
 const (
-	CFGFile         = "testconfig.json"
-	ClientTLSConfig = "client-config.json"
-	FCADB           = "../../testdata/fabric-ca.db"
+	CFGFile      = "testconfig.json"
+	ClientConfig = "../../testdata/client-config.json"
+	FCADB        = "../../testdata/fabric-ca.db"
 )
 
 var serverStarted bool
@@ -83,9 +82,6 @@ func TestPostgresFail(t *testing.T) {
 
 func TestRegisterUser(t *testing.T) {
 	startServer()
-	clientConfig := filepath.Join(dir, ClientTLSConfig)
-	os.Link("../../testdata/client-config2.json", clientConfig)
-
 	c := getClient(t)
 	if c == nil {
 		return
@@ -212,8 +208,7 @@ func TestRevoke(t *testing.T) {
 }
 
 func TestGetTCerts(t *testing.T) {
-	fcaServer := `{"serverURL":"https://localhost:8888"}`
-	c, err := lib.NewClient(fcaServer)
+	c, err := lib.NewClient(ClientConfig)
 	if err != nil {
 		t.Errorf("TestGetTCerts.NewClient failed: %s", err)
 		return
@@ -293,15 +288,18 @@ func TestEnroll(t *testing.T) {
 }
 
 func testUnregisteredUser(t *testing.T) {
-	fcaServer := `{"serverURL":"https://localhost:8888"}`
-	c, _ := lib.NewClient(fcaServer)
+	c, err := lib.NewClient(ClientConfig)
+	if err != nil {
+		t.Errorf("Failed to create client [error: %s]", err)
+		return
+	}
 
 	req := &api.EnrollmentRequest{
 		Name:   "Unregistered",
 		Secret: "test",
 	}
 
-	_, err := c.Enroll(req)
+	_, err = c.Enroll(req)
 
 	if err == nil {
 		t.Error("Unregistered user should not be allowed to enroll, should have failed")
@@ -309,15 +307,18 @@ func testUnregisteredUser(t *testing.T) {
 }
 
 func testIncorrectToken(t *testing.T) {
-	fcaServer := `{"serverURL":"https://localhost:8888"}`
-	c, _ := lib.NewClient(fcaServer)
+	c, err := lib.NewClient(ClientConfig)
+	if err != nil {
+		t.Errorf("Failed to create client [error: %s]", err)
+		return
+	}
 
 	req := &api.EnrollmentRequest{
 		Name:   "notadmin",
 		Secret: "pass1",
 	}
 
-	_, err := c.Enroll(req)
+	_, err = c.Enroll(req)
 
 	if err == nil {
 		t.Error("Incorrect token should not be allowed to enroll, should have failed")
@@ -325,15 +326,18 @@ func testIncorrectToken(t *testing.T) {
 }
 
 func testEnrollingUser(t *testing.T) {
-	fcaServer := `{"serverURL":"https://localhost:8888"}`
-	c, _ := lib.NewClient(fcaServer)
+	c, err := lib.NewClient(ClientConfig)
+	if err != nil {
+		t.Errorf("Failed to create client [error: %s]", err)
+		return
+	}
 
 	req := &api.EnrollmentRequest{
 		Name:   "testUser2",
 		Secret: "user2",
 	}
 
-	_, err := c.Enroll(req)
+	_, err = c.Enroll(req)
 
 	if err != nil {
 		t.Error("Enroll of user 'testUser2' with password 'user2' failed")
@@ -360,9 +364,11 @@ func TestRevokeCertificatesByID(t *testing.T) {
 }
 
 func TestExpiration(t *testing.T) {
-
-	fcaServer := `{"serverURL":"https://localhost:8888"}`
-	c, _ := lib.NewClient(fcaServer)
+	c, err := lib.NewClient(ClientConfig)
+	if err != nil {
+		t.Errorf("Failed to create client [error: %s]", err)
+		return
+	}
 
 	// Enroll this user using the "expiry" profile which is configured
 	// to expire after 1 second
@@ -410,29 +416,6 @@ func TestUserRegistry(t *testing.T) {
 
 }
 
-func TestCreateHome(t *testing.T) {
-	s := createServer()
-	t.Log("Test Creating Home Directory")
-	os.Unsetenv("FABRIC_CA_HOME")
-	tempDir, err := ioutil.TempDir("", "test")
-	if err != nil {
-		t.Errorf("Failed to create temp directory [error: %s]", err)
-	}
-	os.Setenv("HOME", tempDir)
-
-	_, err = s.CreateHome()
-	if err != nil {
-		t.Errorf("Failed to create home directory, error: %s", err)
-	}
-
-	if _, err = os.Stat(dir); err != nil {
-		if os.IsNotExist(err) {
-			t.Error("Failed to create home directory")
-		}
-	}
-
-}
-
 func TestLast(t *testing.T) {
 	// Cleanup
 	os.Remove(FCADB)
@@ -459,8 +442,7 @@ func testWithoutAuthHdr(c *lib.Client, t *testing.T) {
 }
 
 func getClient(t *testing.T) *lib.Client {
-	fcaServer := `{"serverURL":"https://localhost:8888"}`
-	c, err := lib.NewClient(fcaServer)
+	c, err := lib.NewClient(ClientConfig)
 	if err != nil {
 		t.Fatalf("TestMisc.NewClient failed: %s", err)
 		return nil
