hyperledger-gerrit-archive/fabric-cop

Fixes unit-tests for Vagrant
os.Link to "copy" the config file to /tmp fails on vagrant because it
crosses over different file systems. This patch simply copies the file
instead.

Before:
2017/01/17 15:12:43 [DEBUG] Retrieving client config
--- FAIL: TestEnrollCLI (10.01s)
        client_test.go:60: Failed to register, err:  Failed to load client config file [open /tmp/client196787572/cop_client.json: no such file or directory]; not sending
...
FAIL    github.com/hyperledger/fabric-cop/cli/client    10.048s
error: exit status 1

After:
ok      github.com/hyperledger/fabric-cop/cli/client    10.172s coverage: 76.5% of statements

Change-Id: I2a64f93cb62fbc5abb62df6a4cd980702f0e01bc
Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>

cli/client/client_test.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/cloudflare/cfssl/cli"
 	"github.com/hyperledger/fabric-cop/cli/server"
+	"github.com/hyperledger/fabric-cop/util"
 )
 
 var serverStarted bool
@@ -33,6 +34,7 @@ var serverExitCode = 0
 var dir string
 
 const (
+	CFGFile         = "testconfig.json"
 	ClientTLSConfig = "cop_client.json"
 	COPDB           = "../../testdata/cop.db"
 )
@@ -49,13 +51,17 @@ func TestEnrollCLI(t *testing.T) {
 	startServer()
 
 	clientConfig := filepath.Join(dir, ClientTLSConfig)
-	os.Link("../../testdata/cop_client2.json", clientConfig)
+	config, _ := filepath.Abs("../../testdata/cop_client2.json")
+	err := util.CopyFile(config, clientConfig)
+	if err != nil {
+		t.Error("Failed to create client config file link, err: ", err)
+	}
 
 	c := new(cli.Config)
 
 	args := []string{"admin", "adminpw", "https://localhost:8888"}
 
-	err := enrollMain(args, *c)
+	err = enrollMain(args, *c)
 	if err != nil {
 		t.Error("Failed to register, err: ", err)
 	}
@@ -216,7 +222,7 @@ func TestLast(t *testing.T) {
 
 func runServer() {
 	os.Setenv("COP_DEBUG", "true")
-	server.Start("../../testdata", "testconfig.json")
+	server.Start("../../testdata", CFGFile)
 }
 
 func startServer() {

cli/server/server_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/hyperledger/fabric-cop/cli/server/dbutil"
 	"github.com/hyperledger/fabric-cop/cli/server/ldap"
 	"github.com/hyperledger/fabric-cop/lib"
+	"github.com/hyperledger/fabric-cop/util"
 )
 
 const (
@@ -83,8 +84,13 @@ func TestPostgresFail(t *testing.T) {
 
 func TestRegisterUser(t *testing.T) {
 	startServer()
+
 	clientConfig := filepath.Join(dir, ClientTLSConfig)
-	os.Link("../../testdata/cop_client2.json", clientConfig)
+	config, _ := filepath.Abs("../../testdata/cop_client2.json")
+	err := util.CopyFile(config, clientConfig)
+	if err != nil {
+		t.Error("Failed to create client config file link, err: ", err)
+	}
 
 	c := getClient(t)
 	if c == nil {

lib/client_test.go
@@ -29,9 +29,11 @@ import (
 	"github.com/cloudflare/cfssl/log"
 	"github.com/hyperledger/fabric-cop/api"
 	"github.com/hyperledger/fabric-cop/cli/server"
+	"github.com/hyperledger/fabric-cop/util"
 )
 
 const (
+	CFGFile         = "testconfig.json"
 	ClientTLSConfig = "cop_client.json"
 	COPDB           = "../testdata/cop.db"
 )
@@ -44,7 +46,11 @@ func TestAllClient(t *testing.T) {
 	startServer()
 
 	clientConfig := filepath.Join(dir, ClientTLSConfig)
-	os.Link("../testdata/cop_client.json", clientConfig)
+	config, _ := filepath.Abs("../testdata/cop_client.json")
+	err := util.CopyFile(config, clientConfig)
+	if err != nil {
+		t.Error("Failed to create client config file link, err: ", err)
+	}
 
 	c := getClient()
 
@@ -252,7 +258,7 @@ func startServer() int {
 func runServer() {
 	os.Setenv("COP_DEBUG", "true")
 	os.Setenv("COP_HOME", dir)
-	server.Start("../testdata", "testconfig.json")
+	server.Start("../testdata", CFGFile)
 }
 
 func TestLast(t *testing.T) {

hyperledger-gerrit-archive/fabric-ca

cli/client/client_test.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/cloudflare/cfssl/cli"
 	"github.com/hyperledger/fabric-ca/cli/server"
+	"github.com/hyperledger/fabric-ca/util"
 )
 
 var serverStarted bool
@@ -33,6 +34,7 @@ var serverExitCode = 0
 var dir string
 
 const (
+	CFGFile         = "testconfig.json"
 	ClientTLSConfig = "client-config.json"
 	FabricCADB      = "../../testdata/fabric-ca.db"
 )
@@ -49,13 +51,17 @@ func TestEnrollCLI(t *testing.T) {
 	startServer()
 
 	clientConfig := filepath.Join(dir, ClientTLSConfig)
-	os.Link("../../testdata/client-config2.json", clientConfig)
+	config, _ := filepath.Abs("../../testdata/client-config2.json")
+	err := util.CopyFile(config, clientConfig)
+	if err != nil {
+		t.Error("Failed to create client config file link, err: ", err)
+	}
 
 	c := new(cli.Config)
 
 	args := []string{"admin", "adminpw", "https://localhost:8888"}
 
-	err := enrollMain(args, *c)
+	err = enrollMain(args, *c)
 	if err != nil {
 		t.Error("Failed to register, err: ", err)
 	}
@@ -216,7 +222,7 @@ func TestLast(t *testing.T) {
 
 func runServer() {
 	os.Setenv("FABRIC_CA_DEBUG", "true")
-	server.Start("../../testdata", "testconfig.json")
+	server.Start("../../testdata", CFGFile)
 }
 
 func startServer() {

cli/server/server_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/hyperledger/fabric-ca/cli/server/dbutil"
 	"github.com/hyperledger/fabric-ca/cli/server/ldap"
 	"github.com/hyperledger/fabric-ca/lib"
+	"github.com/hyperledger/fabric-ca/util"
 )
 
 const (
@@ -83,8 +84,13 @@ func TestPostgresFail(t *testing.T) {
 
 func TestRegisterUser(t *testing.T) {
 	startServer()
+
 	clientConfig := filepath.Join(dir, ClientTLSConfig)
-	os.Link("../../testdata/client-config2.json", clientConfig)
+	config, _ := filepath.Abs("../../testdata/client-config2.json")
+	err := util.CopyFile(config, clientConfig)
+	if err != nil {
+		t.Error("Failed to create client config file link, err: ", err)
+	}
 
 	c := getClient(t)
 	if c == nil {

lib/client_test.go
@@ -29,9 +29,11 @@ import (
 	"github.com/cloudflare/cfssl/log"
 	"github.com/hyperledger/fabric-ca/api"
 	"github.com/hyperledger/fabric-ca/cli/server"
+	"github.com/hyperledger/fabric-ca/util"
 )
 
 const (
+	CFGFile         = "testconfig.json"
 	ClientTLSConfig = "client-config.json"
 	FCADB           = "../testdata/fabric-ca.db"
 )
@@ -44,7 +46,11 @@ func TestAllClient(t *testing.T) {
 	startServer()
 
 	clientConfig := filepath.Join(dir, ClientTLSConfig)
-	os.Link("../testdata/client-config.json", clientConfig)
+	config, _ := filepath.Abs("../testdata/client-config.json")
+	err := util.CopyFile(config, clientConfig)
+	if err != nil {
+		t.Error("Failed to create client config file link, err: ", err)
+	}
 
 	c := getClient()
 
@@ -252,7 +258,7 @@ func startServer() int {
 func runServer() {
 	os.Setenv("FABRIC_CA_DEBUG", "true")
 	os.Setenv("FABRIC_CA_HOME", dir)
-	server.Start("../testdata", "testconfig.json")
+	server.Start("../testdata", CFGFile)
 }
 
 func TestLast(t *testing.T) {