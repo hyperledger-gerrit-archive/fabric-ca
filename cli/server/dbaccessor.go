hyperledger-gerrit-archive/fabric-cop

COP server throws NPE using config file to start
This reintroduces code that checks the config file
to see if CA Cert and CA Key are provided in the config
and uses those to start the server. However, if Cert and Key
are provided on CLI as well, CLI will take precedence over what is
defined in the COP configuration file.

Test cases enhanced to test both scenarios of starting
server from configuration file and cli.

https://jira.hyperledger.org/browse/FAB-1596

Change-Id: Iac6ca5e0a488032238b92ac327edbf8b411aeaf0
Signed-off-by: Saad Karim <skarim@us.ibm.com>

cli/server/dbaccessor.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cloudflare/cfssl/log"
 	"github.com/hyperledger/fabric-cop/api"
 	"github.com/hyperledger/fabric-cop/cli/server/spi"
+	"github.com/hyperledger/fabric-cop/util"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/kisielk/sqlstruct"
@@ -329,7 +330,7 @@ func (d *Accessor) GetRootGroup() (spi.Group, error) {
 		return nil, err
 	}
 	// TODO: IMPLEMENT
-	return nil, errors.New("NOT YET IMPLEMENTED")
+	return nil, util.ErrNotImplemented
 }
 
 // Creates a DBUser object from the DB user record

hyperledger-gerrit-archive/fabric-ca

cli/server/dbaccessor.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cloudflare/cfssl/log"
 	"github.com/hyperledger/fabric-ca/api"
 	"github.com/hyperledger/fabric-ca/cli/server/spi"
+	"github.com/hyperledger/fabric-ca/util"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/kisielk/sqlstruct"
@@ -329,7 +330,7 @@ func (d *Accessor) GetRootGroup() (spi.Group, error) {
 		return nil, err
 	}
 	// TODO: IMPLEMENT
-	return nil, errors.New("NOT YET IMPLEMENTED")
+	return nil, util.ErrNotImplemented
 }
 
 // Creates a DBUser object from the DB user record