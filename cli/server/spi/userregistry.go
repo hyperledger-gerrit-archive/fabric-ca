hyperledger-gerrit-archive/fabric-cop

Replace group with affiliation path for users
Convert "group" to "affiliation_path" in the users table.
Affiliation path will have the whole group hierarchy
represented for each user.

https://jira.hyperledger.org/browse/FAB-1511

Change-Id: I91025dea3aecd9a72727dc8cf3b126e34cd85e02
Signed-off-by: Saad Karim <skarim@us.ibm.com>

cli/server/spi/userregistry.go
@@ -25,13 +25,13 @@ import "github.com/hyperledger/fabric-cop/idp"
 
 // UserInfo contains information about a user
 type UserInfo struct {
-	Name           string
-	Pass           string
-	Type           string
-	Group          string
-	Attributes     []idp.Attribute
-	State          int
-	MaxEnrollments int
+	Name            string
+	Pass            string
+	Type            string
+	AffiliationPath string
+	Attributes      []idp.Attribute
+	State           int
+	MaxEnrollments  int
 }
 
 // GroupInfo defines a group name and its parent

hyperledger-gerrit-archive/fabric-ca

cli/server/spi/userregistry.go
@@ -24,13 +24,13 @@ import "github.com/hyperledger/fabric-ca/api"
 
 // UserInfo contains information about a user
 type UserInfo struct {
-	Name           string
-	Pass           string
-	Type           string
-	Group          string
-	Attributes     []api.Attribute
-	State          int
-	MaxEnrollments int
+	Name            string
+	Pass            string
+	Type            string
+	AffiliationPath string
+	Attributes      []api.Attribute
+	State           int
+	MaxEnrollments  int
 }
 
 // GroupInfo defines a group name and its parent