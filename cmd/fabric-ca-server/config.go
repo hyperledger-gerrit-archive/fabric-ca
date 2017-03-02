4802541021d50b53883971fe716483f8c73858a8
Volodymyr Paprotski <vpaprots@ca.ibm.com>
2017-03-02 02:38:13 -0500

    [FAB-2601] Fabric CA BCCSP integration
    This looks like a large change but it generally falls under:
    - BCCSP Keys internally used, instead of []byte
        - import into bccsp if given a PEM
    - add config integration
    - add CFSSL integration
    - test fixes
    
    Change-Id: I491769804c71d02b4a886872de8da5d0adf61944
    Signed-off-by: Volodymyr Paprotski <vpaprots@ca.ibm.com>
    Signed-off-by: Keith Smith <bksmith@us.ibm.com>

--- cmd/fabric-ca-server/config.go
+++ cmd/fabric-ca-server/config.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -237,14 +238,17 @@ csr:
       expiry:
 
 #############################################################################
-#  Crypto section configures the crypto primitives used for all
+# BCCSP (BlockChain Crypto Service Provider) section allows to select which
+# crypto implementation library to use
 #############################################################################
-crypto:
-  software:
-     hash_family: SHA2
-     security_level: 256
-     ephemeral: false
-     key_store_dir: keys
+bccsp:
+    default: SW
+    sw:
+        hash: SHA2
+        security: 256
+        filekeystore:
+            # The directory used for the software file-based keystore
+            keystore: <<<KEYSTOREDIR>>>
 `
 )
 
@@ -337,13 +341,17 @@ func createDefaultConfigFile() error {
 	}
 	// Get domain name
 	mydomain := strings.Join(strings.Split(myhost, ".")[1:], ".")
-	// Do string subtitution to get the default config
+	// Get keystoredir
+	cfgDir := filepath.Dir(cfgFileName)
+	keystoredir := path.Join(cfgDir, "keystore")
+	// Do string substitution to get the default config
 	cfg := strings.Replace(defaultCfgTemplate, "<<<ADMIN>>>", user, 1)
 	cfg = strings.Replace(cfg, "<<<ADMINPW>>>", pass, 1)
 	cfg = strings.Replace(cfg, "<<<MYHOST>>>", myhost, 1)
 	cfg = strings.Replace(cfg, "<<<CANAME>>>", mydomain, 1)
+	cfg = strings.Replace(cfg, "<<<KEYSTOREDIR>>>", keystoredir, 1)
 	// Now write the file
-	err = os.MkdirAll(filepath.Dir(cfgFileName), 0755)
+	err = os.MkdirAll(cfgDir, 0755)
 	if err != nil {
 		return err
 	}
