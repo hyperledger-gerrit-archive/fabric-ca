2562168a074f61308d9ce3f338f85d208ac40697
rennman <eabailey@us.ibm.com>
2017-03-03 03:08:59 -0500

    FAB-2571: FVT for split client/server executables
    Updated the existing fvt tests for the new cli commands.
    
    So that the fvt can start running in the CI, tests that are
    expected to fail have been placed in staging directory.
    This includes tests that
     - are currently failing with known issues
     - need additional code to function properly
    Hopefully, this will make it easier to en/disable tests
    in the future.
    
    Deleted some convenience stand-alone script files and put
    the functionality into the fabric-ca_utils file .
    
    Deleted tests that have been obsoleted by the new configuration.
    
    Updated the Makefile to re-enable the docker-fvt images.
    
    Change-Id: I64a21d308db2be8dd2a9be93ee46a52b1c29784c
    Signed-off-by: rennman <eabailey@us.ibm.com>
    
    Improvements to revoke client side command
    
    Revocation logic improved to check that revoker has
    the necessary affiliation to revoke a user.
    
    Check added to make sure that either the enrollment ID
    is provided or both serial and AKI are provided, all three
    can't be provided at the same time.
    
    Storing both serial and AKI in database in hex format.
    This improves user experience as openssl returns both serial
    and AKI in hex format. User's can now directly take the values
    and pass into the revoke command without having to do any
    manipulation.
    
    Test cases added to test various use cases for revocation
    
    https://jira.hyperledger.org/browse/FAB-2517
    
    Change-Id: Ice68cfcc7b134046a18d2124ec3c3b0b1ae8be68
    Signed-off-by: Saad Karim <skarim@us.ibm.com>
    Signed-off-by: Keith Smith <bksmith@us.ibm.com>
    
    FAB-2571: FVT for split client/server executables
    
    Updated the existing fvt tests for the new cli commands.
    
    So that the fvt can start running in the CI, tests that are
    expected to fail have been placed in staging directory.
    This includes tests that
     - are currently failing with known issues
     - need additional code to function properly
    Hopefully, this will make it easier to en/disable tests
    in the future.
    
    Deleted some convenience stand-alone script files and put the
    functionality into the fabric-ca_utils file . Deleted tests that
    have been obsoleted by the new configuration.
    
    Updated the Makefile to re-enable the docker-fvt images.
    
    Updated the scripts/run_ldap_tests to fix the re-located ldap directory.
    
    Enabled all of the backend DB variations.
    
    Change-Id: I337599c663cf6dacc3c9644ade76a01fc4a1ff17
    Signed-off-by: Allen Bailey <eabailey@us.ibm.com>

--- images/fabric-ca-fvt/start.sh
+++ images/fabric-ca-fvt/start.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-su postgres -c 'postgres -D /usr/local/pgsql/data' &
-timeout=10
-i=0
-while ! nc -zvnt -w 5 127.0.0.1 5432; do
- sleep 1
- if test $i -gt $timeout; then break; fi;
- let i++;
-done
+POSTGRES_PORT=5432
+MYSQL_PORT=3306
 
-/usr/bin/mysqld_safe &
-i=0
-while ! nc -zvnt -w 5 127.0.0.1 3306; do
- sleep 1
- if test $i -gt $timeout; then break; fi;
- let i++;
+su postgres -c 'postgres -D /usr/local/pgsql/data' &
+/usr/bin/mysqld_safe --sql-mode=STRICT_TRANS_TABLES &
+
+timeout=15
+for port in $POSTGRES_PORT $MYSQL_PORT; do
+   i=0
+   while ! nc -zvnt -w 5 127.0.0.1 $port; do
+      sleep 1
+      if test $i -gt $timeout; then break; fi;
+      let i++;
+   done
 done
 
 exec "$@"
