c25c2de75e41f84ad3a3600494f5b66fc4d2407e
rennman <eabailey@us.ibm.com>
2017-04-11 13:57:43 -0400

    [ FAB-1892 ] - Add LDAP to fvt test image
    run_ldap_tests is a one-off. Add LDAP funtionality to
    base FVT image so that ldap test can be absorbed into
    CI environment.
    
    Moreover, given the availability of the fabric-ca-fvt
    docker image, it takes about 1 second to run the ldap test.
    
    Change-Id: Ie53f1f3a296318f2bba4c993cdf8298b1fcf0ab5
    Signed-off-by: rennman <eabailey@us.ibm.com>

--- images/fabric-ca-fvt/start.sh
+++ images/fabric-ca-fvt/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 POSTGRES_PORT=5432
 MYSQL_PORT=3306
-PORTS=($POSTGRES_PORT $MYSQL_PORT)
+LDAP_PORT=10389
+PORTS=($POSTGRES_PORT $MYSQL_PORT $LDAP_PORT)
 
 timeout=12
 su postgres -c 'postgres -D /usr/local/pgsql/data' &
 /usr/bin/mysqld_safe --sql-mode=STRICT_TRANS_TABLES &
+/etc/init.d/slapd start &
 
 for port in ${PORTS[*]}; do
    i=0
