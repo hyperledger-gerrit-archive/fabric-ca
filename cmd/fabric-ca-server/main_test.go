c58897aa7909358171ee387d4683eaf54448dd76
Keith Smith <bksmith@us.ibm.com>
2017-05-16 16:36:33 -0400

    [FAB-3415] Improve LDAP usage and error handling
    1) Do not try to load users into the DB when LDAP is enabled.
    2) Don't require the -b option when LDAP is enabled.
    
    Change-Id: Ic673d628e43b057381ea3af29d64d60cd9834847
    Signed-off-by: Keith Smith <bksmith@us.ibm.com>

--- cmd/fabric-ca-server/main_test.go
+++ cmd/fabric-ca-server/main_test.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"regexp"
 	"testing"
@@ -128,6 +129,17 @@ func TestErrors(t *testing.T) {
 	// We are done with all error cases. Now, set the ca.name default value to
 	// "acme.com", as ca.name is a required property for server to start
 	viper.SetDefault("ca.name", "acme.com")
+}
+
+func TestLDAP(t *testing.T) {
+	testDir := "ldap-test"
+	os.RemoveAll(testDir)
+	defer os.RemoveAll(testDir)
+	err := RunMain([]string{cmdName, "init", "-c",
+		path.Join(testDir, "config.yaml"), "--ldap.enabled", "--ldap.url", "ldap://CN=admin@localhost:389/dc=example,dc=com"})
+	if err != nil {
+		t.Errorf("Failed to start server with LDAP enabled: %s", err)
+	}
 }
 
 func TestValid(t *testing.T) {
