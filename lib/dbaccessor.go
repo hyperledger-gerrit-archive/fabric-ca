3818d73de75c61922acab481d0b6c787573d26b6
Gari Singh <gari.r.singh@gmail.com>
2017-06-10 18:32:18 -0400

    [FAB-4515] Fix concurrency issue with enroll
    There was an issue with the logic being used
    to update the state of a user (which
    reflects the number of times the user
    has enrolled).  In the case of MySQL, this
    resulted in the error in the [FAB-4515] but
    also allowed for a user to exceed max
    enrollments when using sqlite3 as the
    database.
    
    The fix is pretty straightforward - it moves
    the logic into the SQL statement in order to
    avoid / depend on locks and or the concurrency
    mode set for a database.
    
    Change-Id: Ifeee607049b8104d2b5eb30bb00a8158ff149671
    Signed-off-by: Gari Singh <gari.r.singh@gmail.com>

--- lib/dbaccessor.go
+++ lib/dbaccessor.go
@@ -371,7 +371,7 @@ func (u *DBUser) Login(pass string, caMaxEnrollments int) error {
 
 	// Not exceeded, so attempt to increment the count
 	state := u.State + 1
-	res, err := u.db.Exec(u.db.Rebind("UPDATE users SET state = ? WHERE (id = ?)"), state, u.Name)
+	res, err := u.db.Exec(u.db.Rebind("UPDATE users SET state = state + 1 WHERE (id = ? AND state < ?)"), u.Name, u.MaxEnrollments)
 	if err != nil {
 		return fmt.Errorf("Failed to update state of identity %s to %d: %s", u.Name, state, err)
 	}
