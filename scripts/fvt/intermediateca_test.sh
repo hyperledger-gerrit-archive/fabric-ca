864eee4c5207651b4e1ad07e0f809e27201ba636
rennman <eabailey@us.ibm.com>
2017-06-13 11:20:58 -0400

    [ FAB-4606 ] Bad intermediate CA CSR
    When acting as an intermediate CA, fabric ca server is allocating
    an empty CSR and essentially setting it to not nil if it is nil.
    
    Then the later check in clientconfig.go which checks for nil;
    if it is nil, then the assignment to the client config CSR never happens.
    
    So an empty CSR gets passed around rather than the correct CSR
    that comes from client config.
    
    Consequently, the root server does not recognize this as an
    intermediate ca request. This has repercussions for later checks
    in the code (e.g. is the hf.IntermediateCA flag set)
    
    Fix is to delete these three lines in lib/ca.go
    
    Change-Id: I729dd212bbae09bd08125aef97fad6803ba9af0b
    Signed-off-by: rennman <eabailey@us.ibm.com>

--- scripts/fvt/intermediateca_test.sh
+++ scripts/fvt/intermediateca_test.sh
@@ -4,33 +4,30 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+export driver="sqlite3"
 FABRIC_CA="$GOPATH/src/github.com/hyperledger/fabric-ca"
 SCRIPTDIR="$FABRIC_CA/scripts/fvt"
+
+PATH=/usr/local/bin:$PATH
 . $SCRIPTDIR/fabric-ca_utils
 RC=0
-
-TDIR=intermediateca-tests
-
-mkdir -p $TDIR/root
-cd $TDIR/root
-fabric-ca-server start -b admin:adminpw -d > server.log 2>&1&
-cd ../..
-sleep 1
-
-mkdir -p $TDIR/int1
-cd $TDIR/int1
-fabric-ca-server start -b admin:adminpw -u http://admin:adminpw@localhost:7054 -p 7055 -d > server.log 2>&1&
-cd ../..
-sleep 1
-
-fabric-ca-client getcacert -u http://admin:adminpw@localhost:7055
-test $? -ne 0 && ErrorExit "Failed to talk to intermediate CA1"
-
-fabric-ca-server init -b admin:adminpw -u http://admin:adminpw@localhost:7055 -d
-test $? -eq 0 && ErrorExit "CA2 should have failed to initialize"
-
-$SCRIPTDIR/fabric-ca_setup.sh -R
-
-CleanUp $RC
+rm -rf $TDIR
+setTLS
+TDIR=/tmp/intermediateca-tests
+PROTO="http://"
+ROOT_CA_PORT=7054
+INT1_CA_PORT=7055
+: ${FABRIC_TLS:="false"}
+if $($FABRIC_TLS); then
+   tlsopts="--tls.enabled --tls.certfile $TESTDATA/tls_server-cert.pem --tls.keyfile $TESTDATA/tls_server-key.pem --intermediate.tls.certfiles $TESTDATA/root.pem"
+   PROTO="https://"
+fi
+FABRIC_CA_SERVER_HOME="$TDIR/root" fabric-ca-server start $tlsopts -b admin:adminpw -d 2>&1 | tee $TDIR/root/server.log &
+pollServer fabric-ca-server localhost $ROOT_CA_PORT 10
+FABRIC_CA_SERVER_HOME="$TDIR/int1" fabric-ca-server start $tlsopts -b admin:adminpw -u ${PROTO}admin:adminpw@localhost:$ROOT_CA_PORT -p $INT1_CA_PORT -d 2>&1 | tee $TDIR/int1/server.log &
+pollServer fabric-ca-server localhost $INT1_CA_PORT 10
+FABRIC_CA_SERVER_HOME="$TDIR/int2" fabric-ca-server init $tlsopts -b admin:adminpw -u ${PROTO}admin:adminpw@localhost:$INT1_CA_PORT -d 2>&1
+test $? -eq 0 && let RC+=1
+kill $(ps -x -o pid,comm | awk '$2~/fabric-ca-serve/ {print $1}')
+CleanUp "$RC"
 exit $RC
