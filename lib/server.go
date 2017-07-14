71eb2662dc4e0fdeb5167d261aa63f61012b0a20
Arnaud J Le Hors <lehors@us.ibm.com>
2017-07-13 17:02:55 -0700

    [FAB-5305] Close CA DBs when server stops
    Server.Stop() closes the listener but doesn't close the CA DBs
    which leads (at least on Vagrant on Windows 7) to problems
    exhibited by the unit-tests which fail to clean up the database
    between tests because the file is busy.
    
    This change makes Server.Stop() close all opened CA DBs, ensuring
    the unit-tests can delete them and start the next test from fresh.
    
    Before this change "go test -v -run TestMaxEnrollment" fails with:
    
    === RUN   TestMaxEnrollmentInfinite
    ...
    2017/07/12 00:59:58 [DEBUG] Directing traffic to default CA
    2017/07/12 00:59:58 [DEBUG] DB: Getting identity admin
    2017/07/12 00:59:58 [DEBUG] DB: Login user admin with max enrollments of -1 and state of 8
    2017/07/12 00:59:58 [DEBUG] Max enrollment value (-1) of identity is greater than allowed by CA, using CA max enrollment value of 1
    2017/07/12 00:59:58 [DEBUG] Failed to login 'admin': The identity admin has already enrolled 1 times, it has reached its maximum enrollment allowance
    2017/07/12 00:59:58 [DEBUG] Received response
    statusCode=400 (400 Bad Request)
    --- FAIL: TestMaxEnrollmentLimited (0.55s)
    server_test.go:986: Test max enrollment limited
    server_test.go:999: Enrollment failed, error: Error response from server was: Authorization failure
    panic: runtime error: invalid memory address or nil pointer dereference [recovered]
    panic: runtime error: invalid memory address or nil pointer dereference
    [signal SIGSEGV: segmentation violation code=0x1 addr=0x0 pc=0x4cb657]
    
    goroutine 754 [running]:
    panic(0xaa1440, 0xc42000e0f0)
    /opt/go/src/runtime/panic.go:500 +0x1a1
    testing.tRunner.func1(0xc42007a600)
    
    ...
    
    After the change it completes successfully:
    
    $ go test -v -run TestMaxEnrollment
    === RUN   TestMaxEnrollmentInfinite
    2017/07/14 00:10:43 [INFO] Starting server in home directory: rootDir
    2017/07/14 00:10:43 [DEBUG] CA Home Directory: rootDir
    2017/07/14 00:10:43 [DEBUG] Making server filenames absolute
    ...
    2017/07/14 00:10:57 [DEBUG] Received response
    statusCode=500 (500 Internal Server Error)
    2017/07/14 00:10:57 [DEBUG] Closing CA DBs
    2017/07/14 00:10:57 [INFO] The server closed its listener endpoint
    2017/07/14 00:10:57 [DEBUG] Waiting for server to stop
    2017/07/14 00:10:57 [ERROR] Server has stopped serving: accept tcp [::]:7075: use of closed network connection
    2017/07/14 00:10:58 [DEBUG] Received server stopped message
    --- PASS: TestMaxEnrollmentLimited (11.65s)
    	server_test.go:1001: Test max enrollment limited
    PASS
    ok  	github.com/hyperledger/fabric-ca/lib	14.749s
    
    Patch-set #2+3: removed leftover line from investigation
    Patch-set #4: changed name to a more generic name server.Clean()
    Patch-set #6+7: rebase + check error returned by server.Clean()
    Patch-set #8: rebase + address Keith Smith's comments
    Patch-set #9: rebase + use new error package
    
    Change-Id: Iebce655378933e6cfaef5ae8c6b67d87c5f20f12
    Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>

--- lib/server.go
+++ lib/server.go
@@ -135,7 +135,11 @@ func (s *Server) Start() (err error) {
 // requests in transit to fail, and so is only used for testing.
 // A graceful shutdown will be supported with golang 1.8.
 func (s *Server) Stop() error {
-	err := s.closeListener()
+	err := s.Clean()
+	if err != nil {
+		return err
+	}
+	err = s.closeListener()
 	if err != nil {
 		return err
 	}
@@ -328,7 +332,6 @@ func (s *Server) loadCA(caFile string, renew bool) error {
 	if err != nil {
 		return err
 	}
-
 	return s.addCA(ca)
 }
 
@@ -354,6 +357,25 @@ func (s *Server) addCA(ca *CA) error {
 	}
 	// no conflicts, so add it
 	s.caMap[caName] = ca
+
+	return nil
+}
+
+// Clean closes all CA dabatases
+func (s *Server) Clean() error {
+	log.Debugf("Closing server DBs")
+	// close default CA DB
+	err := s.CA.closeDB()
+	if err != nil {
+		return err
+	}
+	// close other CAs DB
+	for _, c := range s.caMap {
+		err = c.closeDB()
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
