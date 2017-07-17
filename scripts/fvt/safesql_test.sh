e9e3740eb2c2024ff6f8f7bcd1282118d4b08ec3
Jason Clark <jtclark@linux.vnet.ibm.com>
2017-07-17 11:10:21 -0400

    [FAB-1446] Adding the safesql script for FVT scanning.
    Change-Id: Ia535a6fc75c47afebb1bab50d0805779d3a7f4a6
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

---
+++ scripts/fvt/safesql_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set -o pipefail
+FABRIC_CA="$GOPATH/src/github.com/hyperledger/fabric-ca"
+. $SCRIPTDIR/fabric-ca_utils
+RC=0
+
+safesql -v ../lib/ ../lib/dbutil/ #&> safesql_report.log
+
+CleanUp $RC
+exit $RC
