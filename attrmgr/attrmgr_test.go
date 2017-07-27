ff6f4871ed76e28d674bb5a588d4e9bef0df9916
Keith Smith <bksmith@us.ibm.com>
2017-07-27 02:58:20 -0400

    [FAB-5346] 1.1: Role-based access control (#1)
    This is the first change set to add support for RBAC (Role-Based
    Access Control) to fabric-ca.  This will be accomplished by supporting
    attributes in ECerts (Enrollment Certificates).
    This change set introduces the attrmgr component which will be used
    in the following change set to put attributes in the ECert.
    The APIs support encrypted attributes using BCCSP; however, we
    will need a fix to BCCSP to serialize a key before this is really
    supported, so the code is commented out for now.
    
    Change-Id: I573d1f65b97768f6f771710d5911747db1e9b73e
    Signed-off-by: Keith Smith <bksmith@us.ibm.com>

---
+++ attrmgr/attrmgr_test.go
@@ -0,0 +1,128 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+                 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package attrmgr_test
+
+import (
+	"crypto/x509"
+	"testing"
+
+	"github.com/hyperledger/fabric-ca/attrmgr"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestAttrs tests attributes
+func TestAttrs(t *testing.T) {
+	mgr := attrmgr.New(nil)
+	attrs := []attrmgr.Attribute{
+		&Attribute{Name: "attr1", Value: "val1"},
+		&Attribute{Name: "attr2", Value: "val2"},
+		&Attribute{Name: "attr3", Value: "val3"},
+		&Attribute{Name: "attr4", Value: "val4"},
+		&Attribute{Name: "attr5", Value: "val5"},
+	}
+	reqs := []attrmgr.AttributeRequest{
+		&AttributeRequest{Name: "attr1", Require: false, Encrypt: false},
+		&AttributeRequest{Name: "attr2", Require: true, Encrypt: false},
+		&AttributeRequest{Name: "attr3", Require: false, Encrypt: true},
+		&AttributeRequest{Name: "attr4", Require: true, Encrypt: true},
+		&AttributeRequest{Name: "noattr1", Require: false, Encrypt: true},
+		&AttributeRequest{Name: "noattr2", Require: false, Encrypt: false},
+	}
+	// Add the requested attributes to an X509 certificate
+	cert := &x509.Certificate{}
+	secretAttrInfo, err := mgr.ProcessAttributeRequestsForCert(reqs, attrs, cert)
+	if err != nil {
+		t.Fatalf("Failed to ProcessAttributeRequestsForCert: %s", err)
+	}
+	// Get the Attributes object from the cert and secret info
+	at, err := mgr.GetAttributesFromCert(cert, secretAttrInfo)
+	if err != nil {
+		t.Fatalf("Failed to GetAttributesFromCert: %s", err)
+	}
+	// Get names of attributes
+	numAttrs := len(at.Names())
+	assert.True(t, numAttrs == 4, "expecting 4 attributes but found %d", numAttrs)
+
+	// Check individual attributes
+	checkAttr(t, "attr1", "val1", at)
+	checkAttr(t, "attr2", "val2", at)
+	checkAttr(t, "attr3", "val3", at)
+	checkAttr(t, "attr4", "val4", at)
+	checkAttr(t, "attr5", "", at)
+	checkAttr(t, "noattr1", "", at)
+	checkAttr(t, "noattr2", "", at)
+
+	// Negative test case: add required attributes which don't exist
+	reqs = []attrmgr.AttributeRequest{
+		&AttributeRequest{Name: "noattr1", Require: true, Encrypt: true},
+		&AttributeRequest{Name: "noattr2", Require: true, Encrypt: false},
+	}
+	_, err = mgr.ProcessAttributeRequestsForCert(reqs, attrs, cert)
+	assert.Error(t, err)
+
+	// Negative test case: get attributes with bad secret info
+	_, err = mgr.GetAttributesFromCert(cert, "bad secret info")
+	assert.Error(t, err)
+
+	// Negative test case: get attributes with bad public info in cert
+	cert = &x509.Certificate{}
+	mgr.AddAttributesToCert("bad public info", cert)
+	_, err = mgr.GetAttributesFromCert(cert, "")
+	assert.Error(t, err)
+}
+
+func checkAttr(t *testing.T, name, val string, attrs *attrmgr.Attributes) {
+	v, ok, err := attrs.Value(name)
+	assert.NoError(t, err)
+	if val == "" {
+		assert.False(t, attrs.Contains(name), "contains attribute '%s'", name)
+		assert.False(t, ok, "attribute '%s' was found", name)
+	} else {
+		assert.True(t, attrs.Contains(name), "does not contain attribute '%s'", name)
+		assert.True(t, ok, "attribute '%s' was not found", name)
+		assert.True(t, v == val, "incorrect value for '%s'; expected '%s' but found '%s'", name, val, v)
+	}
+}
+
+type Attribute struct {
+	Name, Value string
+}
+
+func (a *Attribute) GetName() string {
+	return a.Name
+}
+
+func (a *Attribute) GetValue() string {
+	return a.Value
+}
+
+type AttributeRequest struct {
+	Name    string
+	Require bool
+	Encrypt bool
+}
+
+func (ar *AttributeRequest) GetName() string {
+	return ar.Name
+}
+
+func (ar *AttributeRequest) IsRequired() bool {
+	return ar.Require
+}
+
+func (ar *AttributeRequest) ShouldEncrypt() bool {
+	return ar.Encrypt
+}
