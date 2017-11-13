334d1c24a6507a9e3d153672df2f382ac6913f5c
Allen Bailey <eabailey@us.ibm.com>
2017-11-13 17:41:42 -0500

    [ FAB-3416 ] Enhance fvt image LDAP attributes
    In anticipation of
    [FAB-3416] Map LDAP attrs to fabric CA attrs
    the ldap server's DIT is updated with user attributes
    that can be mapped to standard fabric-ca attibutes.
    
    Some convenience cmd-line utils will be added to
    the fabric_ca_utils file, including the ability
    to list Ldap users and groups, all members of a
    specific group, add users, delete users, and modify
    user attributes.
    
    Change-Id: I67e21d85a65628a09b7d0890e86a2086c9a48f61
    Signed-off-by: Allen Bailey <eabailey@us.ibm.com>
    (cherry picked from commit 51a86100bcea4430b0f3bb7bbd1e4d2a7ab9c362)

--- scripts/fvt/ldap_test.sh
+++ scripts/fvt/ldap_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 #
 # Copyright IBM Corp. All Rights Reserved.
 #
@@ -46,15 +46,46 @@ for u in ${users2[*]}; do
    checkUserCert $u
 done
 
-# User 'revoker' revokes user the ecert of user 'testUser'
+URI=$PROTO${CA_HOST_ADDRESS}:$PROXY_PORT
+
+# User 'revoker' revokes the ecert of user 'testUser'
 echo "User 'revoker' is revoking the ecert of user 'testUser' ..."
 certFile=$UDIR/testUser/msp/signcerts/cert.pem
 AKI=$(openssl x509 -noout -text -in $certFile |awk '/keyid/ {gsub(/ *keyid:|:/,"",$1);print toupper($0)}')
-SN=$(openssl x509 -noout -serial -in $certFile | awk -F'=' '{print toupper($2)}')
-URI=$PROTO${CA_HOST_ADDRESS}:$PROXY_PORT
+RSN1=$(openssl x509 -noout -serial -in $certFile | awk -F'=' '{print toupper($2)}')
 export FABRIC_CA_CLIENT_HOME=$UDIR/revoker
-$FABRIC_CA_CLIENTEXEC revoke -u $URI -a $AKI -s $SN $TLSOPT
+$FABRIC_CA_CLIENTEXEC revoke -u $URI -a $AKI -s $RSN1 $TLSOPT
 test "$?" -eq 0 || ErrorMsg "User 'revoker' failed to revoke user 'testUser'"
+
+# User 'admin2' revokes the ecert of user 'testUser2'
+echo "User 'admin2' is revoking the ecert of user 'testUser2' ..."
+certFile=$UDIR/testUser2/msp/signcerts/cert.pem
+AKI=$(openssl x509 -noout -text -in $certFile |awk '/keyid/ {gsub(/ *keyid:|:/,"",$1);print toupper($0)}')
+RSN2=$(openssl x509 -noout -serial -in $certFile | awk -F'=' '{print toupper($2)}')
+$FABRIC_CA_CLIENTEXEC revoke -u $URI -a $AKI -s $RSN2 $TLSOPT -H $UDIR/admin2
+test "$?" -eq 0 || ErrorMsg "User 'admin2' failed to revoke user 'testUser2'"
+
+# User 'notadmin' not authorized to revoke
+echo "User 'notadmin' is attempting to revoke the ecert of user 'nonrevoker' ..."
+certFile=$UDIR/nonrevoker/msp/signcerts/cert.pem
+AKI=$(openssl x509 -noout -text -in $certFile |awk '/keyid/ {gsub(/ *keyid:|:/,"",$1);print toupper($0)}')
+SN=$(openssl x509 -noout -serial -in $certFile | awk -F'=' '{print toupper($2)}')
+$FABRIC_CA_CLIENTEXEC revoke -u $URI -a $AKI -s $SN $TLSOPT -H $UDIR/notadmin 2>&1| grep 'does not have authority to revoke'
+test "$?" -eq 0 || ErrorMsg "User 'notadmin' not authorized (hf.Revoker) to revoke 'nonrevoker'"
+
+# User 'admin' can generate crl
+crlfile=$UDIR/admin/msp/crls/crl.pem
+echo "User 'admin' is generating a crl ... "
+$FABRIC_CA_CLIENTEXEC gencrl -u $URI -H $UDIR/admin $TLSOPT
+test "$?" -eq 0 || ErrorMsg "User 'admin' failed to generate a crl"
+revoked=$(openssl crl -in $crlfile -noout -text | egrep -c "$RSN1|$RSN2")
+test "$revoked" -ne 2 && ErrorMsg "Wrong number of revoked certificates in $crlfile"
+
+# User 'notadmin' cannot generate crl'
+echo "User 'notadmin' is attempting to generate a crl ... "
+$FABRIC_CA_CLIENTEXEC gencrl -u $URI -H $UDIR/notadmin $TLSOPT 2>&1| grep 'Authorization failure'
+test "$?" -eq 0 || ErrorMsg "User 'notadmin' should not generate a crl"
+
 
 CleanUp $RC
 exit $RC
