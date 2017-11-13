4194ac1a0f35c843bc1181ddf08e43ce0cf44e49
Allen Bailey <eabailey@us.ibm.com>
2017-11-13 17:41:42 -0500

    [ FAB-3416 ] Enhance fvt image LDAP attributes
    In anticipation of
    [FAB-3416] Map LDAP attrs to fabric CA attrs
    the ldap server's DIT is updated with user attributes
    that can be mapped to standard fabric-ca attibutes.
    
    Some convenience cmd-line utils will be added to
    the fabric_ca_utils file, including the ability
    to list Ldap users and groups, all members of a
    specific group, add users, delete users, and modify
    user attributes.
    
    Change-Id: I67e21d85a65628a09b7d0890e86a2086c9a48f61
    Signed-off-by: Allen Bailey <eabailey@us.ibm.com>
    (cherry picked from commit 51a86100bcea4430b0f3bb7bbd1e4d2a7ab9c362)

--- scripts/fvt/ldap_test.sh
+++ scripts/fvt/ldap_test.sh
@@ -15,7 +15,7 @@ export UDIR="/tmp/users"
 rm -rf $UDIR
 mkdir -p $UDIR
 
-users1=( admin admin2 revoker revoker2 nonrevoker nonrevoker2 notadmin expiryUser testUser testUser2 )
+users1=( rootadmin admin admin2 notadmin tstadmin devadmin revoker2 revoker nonrevoker testUser testUser2 testUser6 testUser8 )
 users2=( testUser3 )
 
 $SCRIPTDIR/fabric-ca_setup.sh -R
@@ -29,10 +29,33 @@ checkUserCert() {
    test "$ATTRS" == "" && ErrorMsg "Failed to find hf.Revoker and uid attributes in certificate for user $USER"
 }
 
+revokeEcert() {
+   admin="$1"
+   user="$2"
+   result="$3"
+
+   certFile=$UDIR/$user/msp/signcerts/cert.pem
+   AKI=$(openssl x509 -noout -text -in $certFile |awk '/keyid/ {gsub(/ *keyid:|:/,"",$1);print toupper($0)}')
+   SN=$(openssl x509 -noout -serial -in $certFile | awk -F'=' '{print toupper($2)}')
+
+   case "$result" in
+      pass) echo "User '$admin' is revoking the ecert of user cert of user '$user' ..."
+            $FABRIC_CA_CLIENTEXEC revoke -u $URI -a $AKI -s $SN $TLSOPT -H $UDIR/$admin 2>&1
+            test "$?" -eq 0 || ErrorMsg "User '$admin' failed to revoke '$user'"
+      ;;
+      fail) echo "User '$admin is attempting to revoke the ecert of user cert of user '$user' ..."
+# Caller does not have authority to act on affiliation
+            #$FABRIC_CA_CLIENTEXEC revoke -u $URI -a $AKI -s $SN $TLSOPT -H $UDIR/$admin 2>&1| grep 'does not have authority to revoke'
+            $FABRIC_CA_CLIENTEXEC revoke -u $URI -a $AKI -s $SN $TLSOPT -H $UDIR/$admin 2>&1| egrep "(does not have authority to (act|revoke)|Authorization failure)"
+            test "$?" -eq 0 || ErrorMsg "User '$admin' not authorized to revoke '$user'"
+      ;;
+    esac
+}
+
 for u in ${users1[*]}; do
    export CA_CFG_PATH=$UDIR
    enroll $u ${u}pw uid,hf.Revoker
-   test $? -ne 0 && ErrorMsg "Failed to enroll $u"
+   test $? -ne 0 && ErrorExit "Failed to enroll $u"
    checkUserCert $u
 done
 
@@ -46,15 +69,40 @@ for u in ${users2[*]}; do
    checkUserCert $u
 done
 
-# User 'revoker' revokes user the ecert of user 'testUser'
-echo "User 'revoker' is revoking the ecert of user 'testUser' ..."
-certFile=$UDIR/testUser/msp/signcerts/cert.pem
-AKI=$(openssl x509 -noout -text -in $certFile |awk '/keyid/ {gsub(/ *keyid:|:/,"",$1);print toupper($0)}')
-SN=$(openssl x509 -noout -serial -in $certFile | awk -F'=' '{print toupper($2)}')
 URI=$PROTO${CA_HOST_ADDRESS}:$PROXY_PORT
-export FABRIC_CA_CLIENT_HOME=$UDIR/revoker
-$FABRIC_CA_CLIENTEXEC revoke -u $URI -a $AKI -s $SN $TLSOPT
-test "$?" -eq 0 || ErrorMsg "User 'revoker' failed to revoke user 'testUser'"
+
+# User 'revoker' revokes the ecert of user 'testUser'
+revokeEcert revoker testUser pass
+# User 'admin2' revokes the ecert of user 'testUser2'
+revokeEcert admin2 testUser2 pass
+# User 'notadmin' not authorized to revoke (non hf.Revoker)
+revokeEcert notadmin nonrevoker fail
+
+# User 'rootadmin' (uid=rootadmin,dc=example,dc=com) can revoke all affiliations
+for user in testUser2 testUser6 testUser8; do
+   revokeEcert rootadmin $user pass
+done
+# User 'tstadmin' (uid=tstadmin,ou=tst,ou=fabric,dc=hyperledeger,dc=example,dc=com)
+# can only revoke members of the 'tst' group
+revokeEcert tstadmin testUser6 pass
+for user in testUser2 testUser8 ; do
+   revokeEcert tstadmin $user fail
+done
+# User 'devadmin' (uid=devadmin,ou=dev,ou=fabric,dc=hyperledeger,dc=example,dc=com)
+# can only revoke members of the 'dev' group
+revokeEcert devadmin testUser8 pass
+for user in testUser2 testUser6 ; do
+   revokeEcert devadmin $user fail
+done
+
+# User 'admin' can generate crl'
+echo "User 'admin' is generating a crl ... "
+$FABRIC_CA_CLIENTEXEC gencrl -u $URI -H $UDIR/admin $TLSOPT
+test "$?" -eq 0 || ErrorMsg "User 'admin' failed to generate a crl"
+# User 'notadmin' cannot generate crl'
+echo "User 'notadmin' is attempting to generate a crl ... "
+$FABRIC_CA_CLIENTEXEC gencrl -u $URI -H $UDIR/notadmin $TLSOPT 2>&1| grep 'Authorization failure'
+test "$?" -eq 0 || ErrorMsg "User 'notadmin' should not generate a crl"
 
 CleanUp $RC
 exit $RC
