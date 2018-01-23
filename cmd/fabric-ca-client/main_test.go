26b4ec4f6ec7d064a5eaefdc95f009271b516509
Saad Karim <skarim@us.ibm.com>
2018-01-23 12:39:32 -0500

    [FAB-7812] Fix the APIs to return info correctly
    APIs corrected to be more RESTful. Also, the response
    structure for the affiliations was modified from a linear
    view of affiliations to a tree structure.
    
    Change-Id: Ie4e7ff9d1d6a7b4f1120aab0766d11452fd21cd8
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- cmd/fabric-ca-client/main_test.go
+++ cmd/fabric-ca-client/main_test.go
@@ -684,6 +684,15 @@ func TestIdentityCmd(t *testing.T) {
 		cmdName, "identity", "add", "testuser1", "--json", `{"secret": "user1pw", "type": "user", "affiliation": "org1", "max_enrollments": 1, "attrs": [{"name:": "hf.Revoker", "value": "false"}]}`})
 	assert.Error(t, err, "Should have failed to add same user twice")
 
+	// Check that the secret got correctly configured
+	err = RunMain([]string{
+		cmdName, "enroll", "-u", "http://testuser1:user1pw@localhost:7090", "-d"})
+	assert.NoError(t, err, "Failed to enroll user 'testuser2'")
+
+	// Enroll admin back to use it credentials for next commands
+	err = RunMain([]string{cmdName, "enroll", "-u", enrollURL})
+	util.FatalError(t, err, "Failed to enroll user")
+
 	// Add user using flags
 	err = RunMain([]string{
 		cmdName, "identity", "add", "testuser2", "--secret", "user2pw", "--type", "user", "--affiliation", ".", "--maxenrollments", "1", "--attrs", "hf.Revoker=true"})
@@ -692,6 +701,20 @@ func TestIdentityCmd(t *testing.T) {
 	// Check that the secret got correctly configured
 	err = RunMain([]string{
 		cmdName, "enroll", "-u", "http://testuser2:user2pw@localhost:7090", "-d"})
+	assert.NoError(t, err, "Failed to enroll user 'testuser2'")
+
+	// Enroll admin back to use it credentials for next commands
+	err = RunMain([]string{cmdName, "enroll", "-u", enrollURL})
+	util.FatalError(t, err, "Failed to enroll user")
+
+	// modify user secret using flags
+	err = RunMain([]string{
+		cmdName, "identity", "modify", "testuser2", "--secret", "user2pw2"})
+	assert.NoError(t, err, "Failed to add user 'testuser2'")
+
+	// Check that the secret got correctly configured
+	err = RunMain([]string{
+		cmdName, "enroll", "-u", "http://testuser2:user2pw2@localhost:7090", "-d"})
 	assert.NoError(t, err, "Failed to enroll user 'testuser2'")
 
 	// Enroll admin back to use it credentials for next commands
