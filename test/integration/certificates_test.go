39572fd5d534effc56130ab4563bf055dcdce8d4
Saad Karim <skarim@us.ibm.com>
2018-04-02 11:57:28 -0400

    [FAB-7238] 3. CLI Input Validation, Time Parsing, Auth
    Validates that the input on the CLI is formatted correctly.
    Logic added to determine correct time based on the inputs,
    handles duration, RFC3339 format, and "now" as inputs for time.
    
    Logic added that checks to see that the caller is authorized
    to request certificates based on the attributes owned by the
    caller.
    
    Change-Id: I519d7b079b8c65475e7b4172621fd378437ab035
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- test/integration/certificates_test.go
+++ test/integration/certificates_test.go
@@ -22,16 +22,20 @@ import (
 	"path/filepath"
 	"testing"
 
-	cmd "github.com/hyperledger/fabric-ca/cmd/fabric-ca-client/command"
+	"github.com/hyperledger/fabric-ca/api"
+	"github.com/hyperledger/fabric-ca/cmd/fabric-ca-client/command"
 	"github.com/hyperledger/fabric-ca/lib"
 	"github.com/hyperledger/fabric-ca/lib/metadata"
 	"github.com/hyperledger/fabric-ca/util"
+	"github.com/stretchr/testify/assert"
 )
 
 const (
 	serverDir  = "testCertificates"
 	serverPort = 7090
 	cmdName    = "fabric-ca-client"
+	rootDir    = "rootDir"
+	clientHome = "clientHome"
 )
 
 var (
@@ -43,7 +47,7 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestListCertificateCmd(t *testing.T) {
+func TestListCertificateCmdNegative(t *testing.T) {
 	os.RemoveAll(serverDir)
 
 	srv := lib.TestGetServer(serverPort, serverDir, "", -1, t)
@@ -55,13 +59,175 @@ func TestListCertificateCmd(t *testing.T) {
 	os.RemoveAll(filepath.Dir(util.GetDefaultConfigFile("fabric-ca-client")))
 
 	// Command should fail if caller has not yet enrolled
-	err = cmd.RunMain([]string{cmdName, "certificate", "list", "-d"})
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d"})
 	util.ErrorContains(t, err, "Enrollment information does not exist", "Should have failed to call command, if caller has not yet enrolled")
 
 	// Enroll a user that will be used for subsequent certificate commands
-	err = cmd.RunMain([]string{cmdName, "enroll", "-u", enrollURL, "-d"})
+	err = command.RunMain([]string{cmdName, "enroll", "-u", enrollURL, "-d"})
 	util.FatalError(t, err, "Failed to enroll user")
 
-	err = cmd.RunMain([]string{cmdName, "certificate", "list", "-d"})
-	util.ErrorContains(t, err, "Not Implemented", "Should fail, not yet implemented")
+	// Test with --revocation flag
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--revocation", "-30d:-15d"})
+	t.Log("Error: ", err)
+	assert.Error(t, err, "Should fail, only one ':' specified need to specify two '::'")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--revocation", "30d::-15d"})
+	t.Log("Error: ", err)
+	assert.Error(t, err, "Should fail, missing +/- on starting duration")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--revocation", "+30d::15d"})
+	t.Log("Error: ", err)
+	assert.Error(t, err, "Should fail, missing +/- on ending duration")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--revocation", "+30d::+15y"})
+	t.Log("Error: ", err)
+	assert.Error(t, err, "Should fail, invalid duration type (y)")
+
+	// Test with --expiration flag
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--expiration", "-30d:-15d"})
+	t.Log("Error: ", err)
+	assert.Error(t, err, "Should fail, only one ':' specified need to specify two '::'")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--expiration", "30d::-15d"})
+	t.Log("Error: ", err)
+	assert.Error(t, err, "Should fail, missing +/- on starting duration")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--expiration", "+30d::15d"})
+	t.Log("Error: ", err)
+	assert.Error(t, err, "Should fail, missing +/- on ending duration")
+}
+
+func TestListCertificateCmdPositive(t *testing.T) {
+	os.RemoveAll(serverDir)
+
+	srv := lib.TestGetServer(serverPort, serverDir, "", -1, t)
+	err := srv.Start()
+	util.FatalError(t, err, "Failed to start server")
+	defer lib.StopAndCleanupServer(t, srv)
+
+	// Enroll a user that will be used for subsequent certificate commands
+	err = command.RunMain([]string{cmdName, "enroll", "-u", enrollURL, "-d"})
+	util.FatalError(t, err, "Failed to enroll user")
+
+	err = command.RunMain([]string{cmdName, "reenroll", "-d"})
+	util.FatalError(t, err, "Failed to enroll user")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d"})
+	assert.NoError(t, err, "Failed to get certificates")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--expiration", "-30d::+15d"})
+	assert.NoError(t, err, "Failed to parse a correctly formatted expiration duration")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--revocation", "-30d::-15d"})
+	assert.NoError(t, err, "Failed to parse a correctly formatted revocation duration")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--expiration", "2018-01-01::2018-01-31"})
+	assert.NoError(t, err, "Failed to parse a correctly formatted expiration date range")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--revocation", "2018-01-01::2018-01-31"})
+	assert.NoError(t, err, "Failed to parse a correctly formatted revocation date range")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--expiration", "2018-01-01T01:00:00Z::2018-01-31T23:00:00Z"})
+	assert.NoError(t, err, "Failed to parse a correctly formatted expiration date range")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--revocation", "2018-01-01T01:00:00Z::2018-01-31T23:00:00Z"})
+	assert.NoError(t, err, "Failed to parse a correctly formatted revocation date range")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--expiration", "now::+15d"})
+	assert.NoError(t, err, "Failed to parse a expiration date range using 'now'")
+
+	err = command.RunMain([]string{cmdName, "certificate", "list", "-d", "--id", "admin", "--revocation", "-15d::now"})
+	assert.NoError(t, err, "Failed to parse a revocation date range using 'now'")
+}
+
+func TestGetCertificatesTimeInput(t *testing.T) {
+	os.RemoveAll(clientHome)
+	defer os.RemoveAll(clientHome)
+
+	var err error
+
+	srv := lib.TestGetRootServer(t)
+	err = srv.Start()
+	util.FatalError(t, err, "Failed to start server")
+	defer lib.StopAndCleanupServer(t, srv)
+
+	client := lib.GetTestClient(7075, clientHome)
+	resp, err := client.Enroll(&api.EnrollmentRequest{
+		Name:   "admin",
+		Secret: "adminpw",
+	})
+	util.FatalError(t, err, "Failed to enroll user 'admin'")
+
+	admin := resp.Identity
+
+	negativeTimeTestCases(t, admin)
+	positiveTimeTestCases(t, admin)
+}
+
+func negativeTimeTestCases(t *testing.T, admin *lib.Identity) {
+	req := &api.GetCertificatesRequest{
+		Expired: api.TimeRange{
+			EndTime: "-30y",
+		},
+	}
+	err := admin.GetCertificates(req, nil)
+	assert.Error(t, err, "Incorrect time frame for expiration end time")
+
+	req = &api.GetCertificatesRequest{
+		Revoked: api.TimeRange{
+			EndTime: "-30y",
+		},
+	}
+	err = admin.GetCertificates(req, nil)
+	assert.Error(t, err, "Incorrect time frame for revocation end time")
+
+	req = &api.GetCertificatesRequest{
+		Revoked: api.TimeRange{
+			EndTime: "-IOd",
+		},
+	}
+	err = admin.GetCertificates(req, nil)
+	assert.Error(t, err, "Incorrect time format")
+
+	req = &api.GetCertificatesRequest{
+		Revoked: api.TimeRange{
+			EndTime: "-30.5",
+		},
+	}
+	err = admin.GetCertificates(req, nil)
+	assert.Error(t, err, "Incorrect time format")
+
+	req = &api.GetCertificatesRequest{
+		Revoked: api.TimeRange{
+			EndTime: "2018-01-01T00:00:00",
+		},
+	}
+	err = admin.GetCertificates(req, nil)
+	assert.Error(t, err, "Incorrect time format")
+}
+
+func positiveTimeTestCases(t *testing.T, admin *lib.Identity) {
+	req := &api.GetCertificatesRequest{
+		Expired: api.TimeRange{
+			StartTime: "+30d",
+		},
+	}
+	err := admin.GetCertificates(req, nil)
+	assert.NoError(t, err, "Failed to parse correct time")
+
+	req = &api.GetCertificatesRequest{
+		Expired: api.TimeRange{
+			StartTime: "2018-01-01",
+		},
+	}
+	err = admin.GetCertificates(req, nil)
+	assert.NoError(t, err, "Failed to parse date/time without the time")
+
+	req = &api.GetCertificatesRequest{
+		Expired: api.TimeRange{
+			StartTime: "2018-01-01T00:00:00Z",
+		},
+	}
+	err = admin.GetCertificates(req, nil)
+	assert.NoError(t, err, "Failed to parse date/time")
 }
