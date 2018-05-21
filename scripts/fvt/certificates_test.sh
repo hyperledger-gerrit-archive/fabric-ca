562674d2a5cbe1be3fb15641914f4596375f5b0b
Saad Karim <skarim@us.ibm.com>
2018-05-21 13:21:46 -0400

    [FAB-10321] Test Certificates API with MySQL
    Testing the new database query added for the
    GET Certificates REST API, which executes a database
    query to retrieve certificates based on certain filters.
    
    This test ensures that the query gets executed properly
    against MySQL and the correct results are returned
    back to the user.
    
    Change-Id: I2908a4861929e97b16be712f92f03f0448b37b4d
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- scripts/fvt/certificates_test.sh
+++ scripts/fvt/certificates_test.sh
@@ -187,3 +187,183 @@ test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command wit
 $SCRIPTDIR/fabric-ca_setup.sh -K
 postgresDBCleanup
 
+#####################################################################
+# Testing Certificates API with PostgreSQL - Complete
+#####################################################################
+
+function mysqlDBCleanup() {
+    mysql --host=localhost --user=root --password=mysql --database=$DBNAME -e "TRUNCATE TABLE certificates" &> /dev/null
+}
+
+function populateMySQLCertsTable() {
+    # Expired and Not Revoked
+    insertMySQLCertsTable "user1" "1111" "2222" "2017/11/18" "0000/00/00"
+    insertMySQLCertsTable "user2" "1112" "2223" "2018/01/18" "0000/00/00"
+    insertMySQLCertsTable "user3" "1111" "2223" "2018/01/18" "0000/00/00"
+    insertMySQLCertsTable "user3" "1111" "2224" "2018/01/18" "0000/00/00"
+    insertMySQLCertsTable "user4" "1113" "2224" "2018/01/25" "0000/00/00"
+
+    # Not Expired and Not Revoked
+    NewDate=$(date "+%Y-%m-%d %H:%M:%S" -d "+20 days")
+    insertMySQLCertsTable "user5" "1114" "2225" "$NewDate" "0000/00/00"
+
+    # Revoked and Not Expired
+    insertMySQLCertsTable "user5" "1115" "2225" "$NewDate" "2018/02/18"
+    insertMySQLCertsTable "user6" "1116" "2225" "$NewDate" "2017/02/18"
+    insertMySQLCertsTable "user7" "1117" "2225" "$NewDate" "2018/01/18"
+
+    # Revoked and Expired
+    insertMySQLCertsTable "user8" "1118" "2225" "2018/01/30" "2018/01/18"
+}
+
+function insertMySQLCertsTable() {
+    local id="$1"
+    local serial="$2"
+    local aki="$3"
+    local expiry="$4"
+    local revokedAt="$5"
+
+    # Generate certificates with the common name set to a user
+    echo "Generating certificate for $id"
+    openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/CN=$id"
+    pem=`cat cert.pem`
+
+    # Store the generated certificate in the certificates table
+    mysql --host=localhost --user=root --password=mysql --database=$DBNAME -e "INSERT INTO certificates (id, serial_number, authority_key_identifier, ca_label, status, reason, expiry, revoked_at, pem, level) VALUES ('$id', '$serial', '$aki', 'ca', 'active', '0', '$expiry', '$revokedAt', '$pem', '1')"
+}
+
+#####################################################################
+# Testing Certificates API with MySQL
+#####################################################################
+
+###### Start Fabric CA Server with MySQL Database #######
+
+mysqlDBCleanup
+$SCRIPTDIR/fabric-ca_setup.sh -I -S -X -D -d mysql
+pollFabricCa
+populateMySQLCertsTable
+
+#### Enroll user first, so subsequent commands can be called ####
+$FABRIC_CA_CLIENTEXEC enroll -u "http://$USERNAME:$USERPSWD@$CA_HOST_ADDRESS:$PROXY_PORT" -H $CA_CFG_PATH/$USERNAME
+if [ $? != 0 ]; then
+    ErrorMsg "Failed to enroll user"
+fi
+
+#### Test various filters for the list certificates commands #####
+
+## List all certificates ##
+$FABRIC_CA_CLIENTEXEC certificate list -H $CA_CFG_PATH/$USERNAME
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command"
+
+## List certificate by ID ##
+
+$FABRIC_CA_CLIENTEXEC certificate list --id user1 -H $CA_CFG_PATH/$USERNAME | grep "user1"
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'id' filter"
+
+## List certificate by Serial Number ##
+
+$FABRIC_CA_CLIENTEXEC certificate list --serial 1111 -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user1" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'serial' filter, user1 certificate not returned"
+grep "user3" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'serial' filter, user3 certificate not returned"
+
+## List certificate by Serial Number and ID ##
+
+$FABRIC_CA_CLIENTEXEC certificate list --serial 1111 --id user1 -H $CA_CFG_PATH/$USERNAME --store $CA_CFG_PATH/$USERNAME> output.txt
+grep "user1" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'serial' filter, user1 certificate not returned"
+grep "user3" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'serial' and 'id' filter, user3 certificate should not be returned"
+if [ ! -f $CA_CFG_PATH/$USERNAME/user1.pem ]; then
+    ErrorMsg "Failed to store certificate in the specified location"
+fi
+
+## List certificate by AKI ##
+
+$FABRIC_CA_CLIENTEXEC certificate list --aki 2223 -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user2" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'aki' filter, user2 certificate not returned"
+grep "user3" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'aki' filter, user3 certificate not returned"
+
+## List certificate by Serial Number, AKI, and ID ##
+
+$FABRIC_CA_CLIENTEXEC certificate list --serial 1111 --aki 2224 --id user3 -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user3" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'serial', 'aki', and 'id' filters, user3 certificate not returned"
+grep "2223" output.txt
+test $? == 1 || ErrorMsg "Incorrectly got certificate for 'user3'"
+
+## List certificate within expiration range ##
+
+$FABRIC_CA_CLIENTEXEC certificate list --expiration 2018-03-01:: -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user5" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'expiration' filter, user5 certificate not returned"
+
+$FABRIC_CA_CLIENTEXEC certificate list --expiration ::2018-01-01 -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user1" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'expiration' filter, user1 certificate not returned"
+grep "user2" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'expiration' filter, user2 certificate should not be returned"
+
+$FABRIC_CA_CLIENTEXEC certificate list --expiration 2018-01-01::2018-03-01 -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user1" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'expiration' filter, user1 certificate should not be returned"
+grep "user2" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'expiration' filter, user2 certificate not returned"
+grep "user3" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'expiration' filter, user3 certificate not returned"
+grep "user4" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'expiration' filter, user4 certificate not returned"
+
+$FABRIC_CA_CLIENTEXEC certificate list --expiration 2018-01-01::2018-03-01 --id user3 -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user2" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'expiration' filter, user2 certificate should not be returned"
+grep "user3" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'expiration' filter, user3 certificate not returned"
+
+## List certificate within revocation range ##
+
+$FABRIC_CA_CLIENTEXEC certificate list --revocation 2018-02-01:: -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user5" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'revocation' filter, user5 certificate not returned"
+
+$FABRIC_CA_CLIENTEXEC certificate list --revocation ::2018-01-01 -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user6" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'revocation' filter, user6 certificate not returned"
+grep "user5" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'revocation' filter, user5 certificate should not be returned"
+
+$FABRIC_CA_CLIENTEXEC certificate list --revocation 2018-01-01::2018-02-01 -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user5" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'revocation' filter, user5 certificate should not be returned"
+grep "user6" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'revocation' filter, user6 certificate should not be returned"
+grep "user7" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'revocation' filter, user7 certificate not returned"
+
+## List certificates within expiration range but have not been revoked ##
+$FABRIC_CA_CLIENTEXEC certificate list --expiration 2018-01-20::2018-01-30 --notrevoked -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user4" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'revocation' and 'notexpired' filter, user4 certificate not returned"
+grep "user8" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'expiration' and 'notrevoked' filter, user8 certificate should not be returned"
+
+## List certificates within revocation range but have not expired ##
+$FABRIC_CA_CLIENTEXEC certificate list --revocation 2018-01-01::2018-01-30 --notexpired -H $CA_CFG_PATH/$USERNAME > output.txt
+grep "user7" output.txt
+test $? == 0 || ErrorMsg "Failed to complete 'certificates list' command with 'revocation' and 'notexpired' filter, user7 certificate not returned"
+grep "user8" output.txt
+test $? == 1 || ErrorMsg "Incorrect results using 'certificate list' command with 'revocation' and 'notexpired' filter, user8 certificate should not be returned"
+
+$SCRIPTDIR/fabric-ca_setup.sh -K
+mysqlDBCleanup
+
+#####################################################################
+# Testing Certificates API with MySQL - Complete
+#####################################################################
+
+rm output.txt
+rm cert.pem
+rm key.pem
