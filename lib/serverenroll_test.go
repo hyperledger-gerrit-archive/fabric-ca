e6a537e96a6b7c67f823b4b38463ba2718f096f4
Anil Ambati <aambati@us.ibm.com>
2018-05-29 11:40:20 -0400

    [FAB-8475] Run migration logic only if db level is old
    Currently, DB migration logic is run during start up even if the
    DB level is same as server level. With this change set, it will
    only be run if the DB level is less than server level. Before this
    change set logic was:
    Update database schema, add users/affiliations from the config file and
    migrate the data
    
    After this change set:
    Database schema is updated and data is migrated in one transaction and
    then users/affiliations defined in the config file are added.
    
    Change-Id: I473453969b034f039dec65ac26d950f1114750c5
    Signed-off-by: Anil Ambati <aambati@us.ibm.com>

--- lib/serverenroll_test.go
+++ lib/serverenroll_test.go
@@ -1,17 +1,7 @@
 /*
-Copyright IBM Corp. 2017 All Rights Reserved.
+Copyright IBM Corp. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-                 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package lib
@@ -21,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/hyperledger/fabric-ca/api"
+	"github.com/hyperledger/fabric-ca/lib/dbutil"
 	"github.com/hyperledger/fabric-ca/util"
 	"github.com/stretchr/testify/assert"
 )
@@ -46,7 +37,7 @@ func TestStateUpdate(t *testing.T) {
 	userInfo, err := registry.GetUser("admin", nil)
 	assert.NoError(t, err, "Failed to get user 'admin' from database")
 	// User state should have gotten updated to 1 after a successful enrollment
-	if userInfo.(*DBUser).State != 1 {
+	if userInfo.(*dbutil.User).State != 1 {
 		t.Error("Incorrect state set for user")
 	}
 
@@ -68,7 +59,7 @@ func TestStateUpdate(t *testing.T) {
 	// State should not have gotten updated because the enrollment failed
 	userInfo, err = registry.GetUser("admin", nil)
 	assert.NoError(t, err, "Failed to get user 'admin' from database")
-	if userInfo.(*DBUser).State != 1 {
+	if userInfo.(*dbutil.User).State != 1 {
 		t.Error("Incorrect state set for user")
 	}
 
--- lib/serveridentities.go
+++ lib/serveridentities.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cloudflare/cfssl/log"
 	"github.com/hyperledger/fabric-ca/api"
 	"github.com/hyperledger/fabric-ca/lib/attr"
+	"github.com/hyperledger/fabric-ca/lib/dbutil"
 	"github.com/hyperledger/fabric-ca/lib/spi"
 	"github.com/hyperledger/fabric-ca/util"
 	"github.com/pkg/errors"
@@ -188,7 +189,7 @@ func getIDs(ctx *serverRequestContextImpl, caller spi.User, caname string) error
 	rowNumber := 0
 	for rows.Next() {
 		rowNumber++
-		var id UserRecord
+		var id dbutil.UserRecord
 		err := rows.StructScan(&id)
 		if err != nil {
 			return newHTTPErr(500, ErrGettingUser, "Failed to get read row: %s", err)
@@ -436,8 +437,8 @@ func processPutRequest(ctx *serverRequestContextImpl, caname string) (*api.Ident
 // Function takes the modification request and fills in missing information with the current user information
 // and parses the modification request to generate the correct input to be stored in the database
 func getModifyReq(user spi.User, req *api.ModifyIdentityRequest) (*spi.UserInfo, bool) {
-	modifyUserInfo := user.(*DBUser).UserInfo
-	userPass := user.(*DBUser).pass
+	modifyUserInfo := user.(*dbutil.User).UserInfo
+	userPass := user.(*dbutil.User).GetPass()
 	setPass := false
 
 	if req.Secret != "" {
@@ -445,6 +446,7 @@ func getModifyReq(user spi.User, req *api.ModifyIdentityRequest) (*spi.UserInfo,
 		modifyUserInfo.Pass = req.Secret
 	} else {
 		modifyUserInfo.Pass = string(userPass)
+
 	}
 
 	if req.MaxEnrollments == -2 {
@@ -469,9 +471,8 @@ func getModifyReq(user spi.User, req *api.ModifyIdentityRequest) (*spi.UserInfo,
 
 	// Update existing attribute, or add attribute if it does not already exist
 	if len(reqAttrs) != 0 {
-		modifyUserInfo.Attributes = getNewAttributes(modifyUserInfo.Attributes, reqAttrs)
+		modifyUserInfo.Attributes = dbutil.GetNewAttributes(modifyUserInfo.Attributes, reqAttrs)
 	}
-
 	return &modifyUserInfo, setPass
 }
 
@@ -494,36 +495,4 @@ func getIDResp(user spi.User, secret, caname string) (*api.IdentityResponse, err
 		Secret:         secret,
 		CAName:         caname,
 	}, nil
-}
-
-// Update existing attribute, or add attribute if it does not already exist
-func getNewAttributes(modifyAttrs, newAttrs []api.Attribute) []api.Attribute {
-	var attr api.Attribute
-	for _, attr = range newAttrs {
-		log.Debugf("Attribute request: %+v", attr)
-		found := false
-		for i := range modifyAttrs {
-			if modifyAttrs[i].Name == attr.Name {
-				if attr.Value == "" {
-					log.Debugf("Deleting attribute: %+v", modifyAttrs[i])
-					if i == len(modifyAttrs)-1 {
-						modifyAttrs = modifyAttrs[:len(modifyAttrs)-1]
-					} else {
-						modifyAttrs = append(modifyAttrs[:i], modifyAttrs[i+1:]...)
-					}
-				} else {
-					log.Debugf("Updating existing attribute from '%+v' to '%+v'", modifyAttrs[i], attr)
-					modifyAttrs[i].Value = attr.Value
-					modifyAttrs[i].ECert = attr.ECert
-				}
-				found = true
-				break
-			}
-		}
-		if !found && attr.Value != "" {
-			log.Debugf("Adding '%+v' as new attribute", attr)
-			modifyAttrs = append(modifyAttrs, attr)
-		}
-	}
-	return modifyAttrs
 }
