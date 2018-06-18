bee5ea252ef5f3a9d7827119d87820660c82e4b6
Saad Karim <skarim@us.ibm.com>
2018-06-18 15:12:01 -0400

    [FAB-8726] Revoke one's own certificate
    Removing the restriction of having the hf.Revoker
    attribute to reovke one's own identity or certificate
    
    Change-Id: Iccb1b0b7611324bc5a7022f35b2d277bd0bd4c87
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- lib/serverrevoke_test.go
+++ lib/serverrevoke_test.go
@@ -7,8 +7,11 @@ SPDX-License-Identifier: Apache-2.0
 package lib
 
 import (
+	"os"
 	"testing"
 
+	"github.com/hyperledger/fabric-ca/api"
+	"github.com/hyperledger/fabric-ca/util"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,4 +23,57 @@ func TestParseInput(t *testing.T) {
 	assert.NotContains(t, parsedInput, ":", "failed to correctly remove colons from input")
 	assert.NotEqual(t, string(parsedInput[0]), "0", "failed to correctly remove leading zeros from input")
 	assert.NotContains(t, parsedInput, "AA", "failed to correctly lowercase capital letters")
+}
+
+func TestRevokeSelf(t *testing.T) {
+	var err error
+	srv := TestGetRootServer(t)
+	err = srv.Start()
+	util.FatalError(t, err, "Failed to start server")
+	defer srv.Stop()
+	defer os.RemoveAll("rootDir")
+	defer os.RemoveAll("../testdata/msp")
+
+	client := getTestClient(7075)
+	resp, err := client.Enroll(&api.EnrollmentRequest{
+		Name:   "admin",
+		Secret: "adminpw",
+	})
+	util.FatalError(t, err, "Failed to enroll user 'admin'")
+
+	admin := resp.Identity
+	name := "testuser"
+	password := "password"
+	_, err = admin.Register(&api.RegistrationRequest{
+		Name:   name,
+		Secret: password,
+	})
+	util.FatalError(t, err, "Failed to register user 'testuser'")
+
+	resp, err = client.Enroll(&api.EnrollmentRequest{
+		Name:   name,
+		Secret: password,
+	})
+	util.FatalError(t, err, "Failed to enroll user 'testuser'")
+	testuser := resp.Identity
+
+	db := srv.CA.CertDBAccessor()
+	cert, err := db.GetCertificatesByID("testuser")
+	util.FatalError(t, err, "Failed to get certificvate for 'testuser'")
+
+	_, err = testuser.Revoke(&api.RevocationRequest{
+		Serial: cert[0].Serial,
+		AKI:    cert[0].AKI,
+	})
+	assert.NoError(t, err, "Failed to revoke one's own certificate using serial and AKI")
+
+	resp, err = client.Enroll(&api.EnrollmentRequest{
+		Name:   name,
+		Secret: password,
+	})
+	util.FatalError(t, err, "Failed to enroll user 'testuser'")
+	testuser = resp.Identity
+
+	_, err = testuser.RevokeSelf()
+	assert.NoError(t, err, "Failed to revoke one self")
 }
