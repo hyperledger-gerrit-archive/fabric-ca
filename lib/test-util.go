1b0bf78b08486b655ef270572399f7f3fca81308
Saad Karim <skarim@us.ibm.com>
2018-11-08 19:00:42 -0500

    FABC-754 Use password object in identity register
    Use the password object to perform password validation or
    generation when registering identities with the fabric
    ca server.
    
    Update test cases to use passwords that meet the new
    default requirements. Also, performed some clean and removal
    of redundant test code.
    
    Change-Id: I936d8c060ce90e9f5484507ff98913fbcfe99433
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- lib/test-util.go
+++ lib/test-util.go
@@ -1,17 +1,7 @@
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
+Copyright IBM Corp. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-                 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package lib
@@ -33,9 +23,12 @@ import (
 	"time"
 
 	"github.com/cloudflare/cfssl/config"
+	"github.com/hyperledger/fabric-ca/api"
 )
 
 const (
+	// Bootstrapadminpw is the password for the test admin user
+	Bootstrapadminpw = "@dminPW1"
 	rootPort         = 7075
 	rootDir          = "rootDir"
 	rootClientDir    = "rootClientDir"
@@ -44,8 +37,14 @@ const (
 	testdataDir      = "../testdata"
 )
 
-func getRootServerURL() string {
-	return fmt.Sprintf("http://admin:adminpw@localhost:%d", rootPort)
+// GetEnrollmentURL returns the non-secure enrollment URL for the test admin user
+func GetEnrollmentURL(port int) string {
+	return fmt.Sprintf("http://admin:%s@localhost:%d", Bootstrapadminpw, port)
+}
+
+// GetSecureEnrollmentURL returns the secure enrollment URL for the test admin user
+func GetSecureEnrollmentURL(port int) string {
+	return fmt.Sprintf("https://admin:%s@localhost:%d", Bootstrapadminpw, port)
 }
 
 // TestGetRootServer creates a server with root configuration
@@ -63,73 +62,40 @@ func TestGetIntermediateServer(idx int, t *testing.T) *Server {
 		t)
 }
 
-// TestGetServer creates and returns a pointer to a server struct
-func TestGetServer(port int, home, parentURL string, maxEnroll int, t *testing.T) *Server {
-	return TestGetServer2(home != testdataDir, port, home, parentURL, maxEnroll, t)
+// TestGetServerActiveDir creates and returns a pointer to a server struct but does not delete the home directory
+func TestGetServerActiveDir(port int, home, parentURL string, maxEnroll int, t *testing.T) *Server {
+	return serverWithDefaultConfig(port, home, parentURL, maxEnroll, t)
 }
 
-// TestGetServer2 creates and returns a pointer to a server struct, with an option of
-// whether or not to remove the home directory first
-func TestGetServer2(deleteHome bool, port int, home, parentURL string, maxEnroll int, t *testing.T) *Server {
-	if deleteHome {
+// TestGetServer creates and returns a pointer to a server struct
+func TestGetServer(port int, home, parentURL string, maxEnroll int, t *testing.T) *Server {
+	if home != testdataDir {
 		os.RemoveAll(home)
 	}
-	affiliations := map[string]interface{}{
-		"hyperledger": map[string]interface{}{
-			"fabric":    []string{"ledger", "orderer", "security"},
-			"fabric-ca": nil,
-			"sdk":       nil,
-		},
-		"org2":      []string{"dept1"},
-		"org1":      nil,
-		"org2dept1": nil,
-	}
-	profiles := map[string]*config.SigningProfile{
-		"tls": &config.SigningProfile{
-			Usage:        []string{"signing", "key encipherment", "server auth", "client auth", "key agreement"},
-			ExpiryString: "8760h",
-		},
-		"ca": &config.SigningProfile{
-			Usage:        []string{"cert sign", "crl sign"},
-			ExpiryString: "8760h",
-			CAConstraint: config.CAConstraint{
-				IsCA:       true,
-				MaxPathLen: 0,
-			},
-		},
-	}
-	defaultProfile := &config.SigningProfile{
-		Usage:        []string{"cert sign"},
-		ExpiryString: "8760h",
+
+	return serverWithDefaultConfig(port, home, parentURL, maxEnroll, t)
+}
+
+// TestServerWithCustomConfig creates and returns a pointer to server using custom configuration
+func TestServerWithCustomConfig(port int, home, parentURL string, maxEnroll int, serverCfg *ServerConfig, caCfg *CAConfig, t *testing.T) *Server {
+	if serverCfg == nil || caCfg == nil {
+		t.Fatal("Either missing server or CA config")
 	}
 	srv := &Server{
-		Config: &ServerConfig{
-			Port:  port,
-			Debug: true,
-		},
-		CA: CA{
-			Config: &CAConfig{
-				Intermediate: IntermediateCA{
-					ParentServer: ParentServer{
-						URL: parentURL,
-					},
-				},
-				Affiliations: affiliations,
-				Registry: CAConfigRegistry{
-					MaxEnrollments: maxEnroll,
-				},
-				Signing: &config.Signing{
-					Profiles: profiles,
-					Default:  defaultProfile,
-				},
-				Version: "1.1.0", // The default test server/ca should use the latest version
-			},
-		},
 		HomeDir: home,
+		Config:  serverCfg,
+		CA: CA{
+			Config: caCfg,
+		},
 	}
+	return BootstrapTestIdentities(srv, t)
+}
+
+// BootstrapTestIdentities bootstraps test identities, creates and returns a pointer to a server struct
+func BootstrapTestIdentities(srv *Server, t *testing.T) *Server {
 	// The bootstrap user's affiliation is the empty string, which
 	// means the user is at the affiliation root
-	err := srv.RegisterBootstrapUser("admin", "adminpw", "")
+	err := srv.RegisterBootstrapUser("admin", Bootstrapadminpw, "")
 	if err != nil {
 		t.Errorf("Failed to register bootstrap user: %s", err)
 		return nil
@@ -213,6 +179,15 @@ func GenerateECDSATestCert() error {
 	return nil
 }
 
+// EnrollDefaultTestBootstrapAdmin enrolls the test admin user
+func EnrollDefaultTestBootstrapAdmin(c *Client) (*EnrollmentResponse, error) {
+	Bootstrapadminpw := "@dminPW1"
+	return c.Enroll(&api.EnrollmentRequest{
+		Name:   "admin",
+		Secret: Bootstrapadminpw,
+	})
+}
+
 // Currently not being used anywhere, commenting it out for right now
 // it was just bringing test coverage.
 
@@ -238,4 +213,65 @@ func TestGetClient(port int, home string) *Client {
 		Config:  &ClientConfig{URL: fmt.Sprintf("http://localhost:%d", port)},
 		HomeDir: home,
 	}
+}
+
+func serverWithDefaultConfig(port int, home, parentURL string, maxEnroll int, t *testing.T) *Server {
+	affiliations := map[string]interface{}{
+		"hyperledger": map[string]interface{}{
+			"fabric":    []string{"ledger", "orderer", "security"},
+			"fabric-ca": nil,
+			"sdk":       nil,
+		},
+		"org2":      []string{"dept1"},
+		"org1":      nil,
+		"org2dept1": nil,
+	}
+	profiles := map[string]*config.SigningProfile{
+		"tls": &config.SigningProfile{
+			Usage:        []string{"signing", "key encipherment", "server auth", "client auth", "key agreement"},
+			ExpiryString: "8760h",
+		},
+		"ca": &config.SigningProfile{
+			Usage:        []string{"cert sign", "crl sign"},
+			ExpiryString: "8760h",
+			CAConstraint: config.CAConstraint{
+				IsCA:       true,
+				MaxPathLen: 0,
+			},
+		},
+	}
+	defaultProfile := &config.SigningProfile{
+		Usage:        []string{"cert sign"},
+		ExpiryString: "8760h",
+	}
+	srv := &Server{
+		Config: &ServerConfig{
+			Port:  port,
+			Debug: true,
+		},
+		CA: CA{
+			Config: &CAConfig{
+				Intermediate: IntermediateCA{
+					ParentServer: ParentServer{
+						URL: parentURL,
+					},
+				},
+				Affiliations: affiliations,
+				Registry: CAConfigRegistry{
+					MaxEnrollments: maxEnroll,
+				},
+				Signing: &config.Signing{
+					Profiles: profiles,
+					Default:  defaultProfile,
+				},
+				Version: "1.1.0", // The default test server/ca should use the latest version
+			},
+		},
+		HomeDir: home,
+	}
+	return BootstrapTestIdentities(srv, t)
+}
+
+func getRootServerURL() string {
+	return GetEnrollmentURL(rootPort)
 }
