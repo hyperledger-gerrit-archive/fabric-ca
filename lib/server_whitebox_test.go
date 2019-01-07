84380192853a9065cfc6239b773f04ead36acfc2
Saad Karim <skarim@us.ibm.com>
2019-01-07 14:02:08 -0500

    [FABC-803] Refactoring DB code
    Refactored some of the database code into 3 distinct
    packages, each package is a databases the CA supports
    (SQLite, MySQL, and PostgresSQL).
    
    There was a lack of proper unit test regarding a lot of the
    database logic. Unit tests added for all the 3 databases.
    
    This restructing of the database code will be useful for adding
    support for metrics.
    
    Change-Id: Idd5ed1c802ac6ba35de76462cbbb0baf0222eceb
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- lib/server_whitebox_test.go
+++ lib/server_whitebox_test.go
@@ -1,18 +1,9 @@
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
+Copyright IBM Corp. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-                 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
+
 package lib
 
 import (
@@ -79,13 +70,16 @@ func TestGetAffliation(t *testing.T) {
 		}
 	}()
 
-	afs := []AffiliationRecord{}
-	err = srv.db.Select(&afs, srv.db.Rebind(getAffiliationQuery), affiliationName)
-	t.Logf("Retrieved %+v for the affiliation %s", afs, affiliationName)
+	name := "org1.department1"
+	rows, err := srv.CA.registry.GetAllAffiliations(name)
 	if err != nil {
 		t.Fatalf("Failed to get affiliation %s: %v", affiliationName, err)
 	}
-	if len(afs) != 1 {
+	var count int
+	for rows.Next() {
+		count++
+	}
+	if count != 1 {
 		t.Fatalf("Found 0 or more than one record for the affiliation %s in the database, expected 1 record", affiliationName)
 	}
 }
