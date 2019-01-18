0a70b3a5489c703d25b40314f066ade50f03f3b5
Brett Logan <Brett.T.Logan@ibm.com>
2019-01-18 09:57:46 -0500

    [FABC-795] Create a CA Server Health Check
    Created the /healthz endpoint on the new Operations
    Server that can be used to gauge the health of the CA
    server. Endpoint returns an http 200 if the Operations
    Server endpoint is available
    
    Change-Id: Iaafb81fc694a9a7b9b5934bceb7c65a1a9094195
    Signed-off-by: Brett Logan <Brett.T.Logan@ibm.com>

--- integration/runner/mysql.go
+++ integration/runner/mysql.go
@@ -9,13 +9,14 @@ package runner
 import (
 	"context"
 	"fmt"
-	"github.com/jmoiron/sqlx"
 	"io"
 	"net"
 	"os"
 	"strconv"
 	"sync"
 	"time"
+
+	"github.com/jmoiron/sqlx"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
@@ -94,7 +95,7 @@ func (c *MySQL) Run(sigCh <-chan os.Signal, ready chan<- struct{}) error {
 	}
 
 	hostConfig := &container.HostConfig{
-		AutoRemove: true,
+		AutoRemove: false,
 		PortBindings: nat.PortMap{
 			"3306/tcp": []nat.PortBinding{
 				{
@@ -182,12 +183,14 @@ func (c *MySQL) Run(sigCh <-chan os.Signal, ready chan<- struct{}) error {
 }
 
 func (c *MySQL) endpointReady(ctx context.Context, db *sqlx.DB) bool {
-	_, err := db.Conn(ctx)
+	conn, err := db.Conn(ctx)
 	if err != nil {
 		return false
 	}
 
+	conn.QueryContext(ctx, "SET GLOBAL sql_mode = '';")
 	db.Close()
+
 	return true
 }
 
@@ -196,7 +199,6 @@ func (c *MySQL) ready(ctx context.Context, addr string) <-chan struct{} {
 
 	str := fmt.Sprintf("root:@(%s:%d)/mysql", c.HostIP, c.HostPort)
 	db, err := sqlx.Open("mysql", str)
-	db.SetConnMaxLifetime(time.Second * 5)
 	if err != nil {
 		ctx.Done()
 	}
